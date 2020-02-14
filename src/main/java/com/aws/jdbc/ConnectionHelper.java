2143a41a49bce300264fcd2a383445b9ec569332
Scott Macdonald <57190223+scmacdon@users.noreply.github.com>
2020-02-14 17:02:37 -0500

    Create ConnectionHelper.java

---
+++ src/main/java/com/aws/jdbc/ConnectionHelper.java
@@ -0,0 +1,43 @@
+package com.aws.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionHelper
+{
+    private String url;
+
+    private static ConnectionHelper instance;
+    private ConnectionHelper()
+    {
+        url = "jdbc:mysql://localhost:3306/mydb";
+    }
+    
+    public static Connection getConnection() throws SQLException {
+        if (instance == null) {
+
+
+            instance = new ConnectionHelper();
+        }
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            return DriverManager.getConnection(instance.url, "root","root");
+        }
+        catch (Exception e) {
+            e.getStackTrace();
+        }
+        return null;
+    }
+    public static void close(Connection connection)
+    {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
